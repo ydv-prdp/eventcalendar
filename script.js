8dbafb409f15180426c5e5854a4168883d5adb07
Pradeep Yadav <pradeepyadav101010@gmail.com>
2023-11-06 06:48:58 +0530

    added goto functionality

--- script.js
+++ script.js
@@ -92,3 +92,34 @@ todayBtn.addEventListener("click",()=>{
     year= today.getFullYear();
     initCalendar();
 })
+
+dateInput.addEventListener("input", (e)=>{
+    dateInput.value = dateInput.value.replace(/[^0-9/]/g, "");
+    if(dateInput.value.length === 2){
+        dateInput.value += "/";
+    }
+    if(dateInput.value.length > 7){
+        dateInput.value = dateInput.value.slice(0, 7);
+    }
+    if(e.inputType === "deleteContentBackward"){
+        if(dateInput.value.length === 3){
+            dateInput.value = dateInput.value.slice(0,2);
+        }
+    }
+})
+
+gotoBtn.addEventListener("click",gotoDate);
+
+
+function gotoDate(){
+    const dateArr = dateInput.value.split("/");
+    if(dateArr.length === 2){
+        if(dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4){
+            month = dateArr[0] - 1;
+            year = dateArr[1];
+            initCalendar();
+            return;
+        }
+    }
+    alert("invalid date");
+}
